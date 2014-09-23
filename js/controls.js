205b53a29bceffa8cb216b372c8fa75578c755b0
Eric <Ericrius1@gmail.com>
2014-09-23 16:36:08 -0700

    camera toggle

--- js/controls.js
+++ js/controls.js
@@ -4,16 +4,22 @@ var Controls = function() {
   var firstTime = true;
   G.fpsControls = new THREE.PointerLockControls(G.camera);
   G.controlObject = G.fpsControls.getObject();
-  // G.controlObject.position.z = -100;
   G.scene.add(G.fpsControls.getObject());
+  
+  G.orbitControls = new THREE.OrbitControls(G.camera, G.renderer.domElement);
+  G.orbitControls.enabled = false;
+
   var mouseTimeoutId;
   var pLockEnabled = false;
-
-  function teleport(point) {
-    G.controlObject.position.set(point);
-  }
+  var activeControls = G.fpsControls;
 
   var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+  
+  // Ask the browser to release the pointer
+document.exitPointerLock = document.exitPointerLock ||
+         document.mozExitPointerLock ||
+         document.webkitExitPointerLock;
+
 
   if (havePointerLock) {
 
@@ -23,7 +29,7 @@ var Controls = function() {
       if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
 
         G.fpsControls.enabled = true;
-        if(!pLockEnabled){
+        if (!pLockEnabled) {
           $('#cursor').addClass('active')
           $(document).on('mousedown', mouseDown);
           $(document).on('mouseup', mouseRelease);
@@ -34,15 +40,15 @@ var Controls = function() {
 
 
 
-
       } else {
+        console.log('washh')
         pLockEnabled = false;
         G.fpsControls.enabled = false;
         $('#cursor').removeClass('active')
-        $(document).off('mousedown');
-        $(document).off('mouseup');
+        $(document).off('mousedown', mouseDown);
+        $(document).off('mouseup', mouseRelease);
 
-        $(document).off('keydown');
+        $(document).off('keydown', keyPressed);
 
 
       }
@@ -50,21 +56,13 @@ var Controls = function() {
     };
 
     var pointerlockerror = function(event) {
-
+      console.log("POINTER LOCK ERROR")
 
     };
-    // Hook pointer lock state change events
-    // document.addEventListener('pointerlockchange', pointerlockchange, false);
-    document.addEventListener('mozpointerlockchange', pointerlockchange, false);
-    document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
 
-    document.addEventListener('pointerlockerror', pointerlockerror, false);
-    document.addEventListener('mozpointerlockerror', pointerlockerror, false);
-    document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+    var requestPointerLock = function() {
 
-    document.addEventListener('click', function(event) {
-
-
+      console.log('CLICK')
 
       // Ask the browser to lock the pointer
       element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
@@ -94,20 +92,30 @@ var Controls = function() {
         element.requestPointerLock();
 
       }
-
-    }, false);
-
+    }
   }
+  // Hook pointer lock state change events
+  document.addEventListener('pointerlockchange', pointerlockchange, false);
+  document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+  document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+
+  document.addEventListener('pointerlockerror', pointerlockerror, false);
+  document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+  document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+
+  document.addEventListener('click', requestPointerLock, false);
+
+
 
   this.update = function() {
-    G.fpsControls.update();
+    activeControls.update();
   }
 
 
   function mouseDown() {
     activePrimitive.spawn();
     $('#cursor').addClass('held');
-    timeoutId = setInterval(function(){
+    timeoutId = setInterval(function() {
       activePrimitive.spawn();
     }, activePrimitive._spawnInterval);
 
@@ -121,14 +129,24 @@ var Controls = function() {
 
   function keyPressed(event) {
     if (event.keyCode in G.primitives) {
-      _.each(G.primitives, function(primitive){
+      _.each(G.primitives, function(primitive) {
         primitive.constructor.$menuItem.removeClass('active')
       })
 
       activePrimitive = G.primitives[event.keyCode];
       activePrimitive.constructor.$menuItem.addClass('active');
 
-     }
+    }
+
+    if (event.keyCode === 67) {
+      document.exitPointerLock()
+      activeControls = G.orbitControls;
+      activeControls.enabled = true;
+      G.fpsControls.enabled = false;
+      G.camera.position.set(0, 0, 30)
+      document.removeEventListener('click', requestPointerLock);
+
+    }
   };
 
 
